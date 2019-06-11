8fa2bf68bc4dd22eba36b4a167b137d4e4f21dc7
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-06-11 14:49:56 -0400

    changed playstore-server functionality

--- playstore-server/app.js
+++ playstore-server/app.js
@@ -1,55 +1,48 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-
+/* eslint-disable quotes */
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(morgan("common"));
+//Express 4.x command for asynch operation
+const listener = app.listen(8000, () => {
+  console.log(`Server is running on port ${listener.address().port}`);
+});
 
-app.use(morgan('common'));
+const playstore = require("./playstore.js");
 
-const playstore = require('./playstore.js');
+app.get("/apps", (req, res) => {
+  const { sort, genres = "" } = req.query;
 
-app.get('./apps', (req, res) => {
-    const {sort, genres} = req.query;
+  let results = playstore;
 
-    let results = playstore;
-
-    if(sort) {
-        if(!['rating', 'app'].includes(sort)) {
-            return res
-            .status(400)
-            .send('Sort must be one of rating or app')
-        }
+  if (sort) {
+    if (!["Rating", "App"].includes(sort)) {
+      return res.status(400).send("Sort must be one of Rating or App");
     }
+  }
 
-    if(sort) {
-        results
-          .sort((a, b) => {
-            return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
-        }); 
-      } 
+  if (sort) {
+    results.sort((a, b) => {
+      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    });
+  }
 
-   
-    if (genres) {
-        if(!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(genres)) {
-            return res
-                .status(400)
-                .send(`Genres must be 'Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'`)
-        }
+  if (genres) {
+    if (
+      ["Action", "Puzzle", "Strategy", "Casual", "Arcade", "Card"].includes(
+        genres
+      )
+    ) {
+      results = playstore.filter(application => {
+        return application.Genres.includes(genres);
+      });
+    } else {
+      res.status(400).send("please enter a valid genre");
     }
-    if(genres) {
-    results = playstore
-            .filter(playstoreapp => {
-                playstoreapp
-                .Genres
-                .toLowerCase()
-                .includes(genres.toLowerCase());
-            })
-    }
+  }
 
-    res
-    .json(results);
-
-   
+  res.json(results);
 });
